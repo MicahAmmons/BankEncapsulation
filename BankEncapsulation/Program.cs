883731b0106822bb599dc246519a176774fca18e
Micah Ammons <micahammons@gmail.com>
2023-11-15 16:09:16 -0500

    Final Init

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How much cash money money dolla bills woul you like to drop?");
+            int userInput;
+            bool isUserInputInt = int.TryParse(Console.ReadLine(), out userInput);
+
+            if (isUserInputInt )
+            {
+                Console.WriteLine($"You are wanting to deposit: {userInput}.  One moment.");
+                Console.Clear();
+                BankAccount account = new BankAccount();
+                account.Balance(userInput);
+                account.GetBalance();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, try again with with an actual number you silly goose.");
+            }
+
         }
     }
 }
